cf7131e572d14cdea076914022472ca933559c97
Markus Hederström <markus.hederstrom@humany.com>
2018-07-20 16:49:44 +0200

    added dropdown to header

--- src/modules/common/components/header.tsx
+++ src/modules/common/components/header.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Dropdown } from 'genui';
 
 import LanguageSelector from './language-selector';
 import { getAuthedUser } from '../../auth/store/selectors';
@@ -11,6 +12,19 @@ export interface HeaderProps {
   containerHeight: number;
   user: User;
 }
+
+const items = [
+  {
+    label: 'Profile',
+    icon: 'profile.png',
+    to: '/profile',
+  },
+  {
+    label: 'Sign out',
+    icon: 'logout.png',
+    to: '/logout',
+  },
+];
 
 class Header extends React.Component<HeaderProps> {
   render() {
@@ -26,11 +40,27 @@ class Header extends React.Component<HeaderProps> {
         </LogoContainer>
 
         <RightNode className="right-node" containerHeight={containerHeight}>
-          <div>{`${user.firstname} ${user.lastname}`}</div>
           <LanguageSelector />
-          <div>
-            <Link to="/logout">Sign out</Link>
-          </div>
+
+          <StyledDropdown
+            className="dropdown"
+            items={items}
+            renderItem={(item: any) => (
+              <Link to={item.to}>
+                <img
+                  src="https://semantic-ui.com/images/avatar/small/jenny.jpg"
+                  alt=""
+                />
+                {item.label}
+              </Link>
+            )}
+          >
+            <img
+              src="https://semantic-ui.com/images/avatar/small/jenny.jpg"
+              alt=""
+            />
+            {`${user.firstname} ${user.lastname}`}
+          </StyledDropdown>
         </RightNode>
       </Container>
     );
@@ -45,6 +75,11 @@ const Container = styled.div`
 
 const LogoContainer = styled.div``;
 
+const StyledDropdown = styled(Dropdown)`
+  line-height: normal;
+  align-self: center;
+`;
+
 interface RightNodeProps {
   containerHeight: number;
 }
@@ -56,8 +91,9 @@ const RightNode = styled.div`
   justify-content: space-between;
 
   ${(props: RightNodeProps) => `line-height: ${props.containerHeight}px;`};
+  ${(props: RightNodeProps) => `height: ${props.containerHeight}px;`};
 
-  div {
+  > div {
     margin: 0 20px;
   }
 `;
