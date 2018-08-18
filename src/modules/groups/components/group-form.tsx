39d70619ff8639d906fd5789fefa749b8d5435b2
Markus Hederström <markus.hederstrom@humany.com>
2018-08-18 14:10:17 +0200

    wip: form validation

--- src/modules/groups/components/group-form.tsx
+++ src/modules/groups/components/group-form.tsx
@@ -3,111 +3,88 @@ import { Field, Input, Button } from 'genui';
 
 import { Project } from '../../projects/store/models';
 import { TimesheetTemplateItem } from '../../timesheets/store/models';
+import { Form, Select } from '../../common';
 
 type Props = {
-  onSubmit: (data: State, projectId: number) => any;
+  onSubmit: (
+    data: {
+      name: string;
+      project: number;
+      timesheetTemplate: TimesheetTemplateItem;
+    },
+    projectId: number
+  ) => any;
   projects: Project[];
   templates: TimesheetTemplateItem[];
 };
 
-type State = Readonly<{
-  name: string;
-  project: number | null;
-  timesheetTemplate: TimesheetTemplateItem | null;
-}>;
+class GroupForm extends React.Component<Props> {
+  handleSubmit = (model: any) => {
+    const data = {
+      ...model,
+      project: +model.project,
+      timesheetTemplate:
+        this.props.templates.find(
+          item => item.id === +model.timesheetTemplate
+        ) || null,
+    };
 
-class GroupForm extends React.Component<Props, State> {
-  readonly state: State = {
-    name: '',
-    project: null,
-    timesheetTemplate: null,
-  };
-
-  handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    const {
-      name,
-      value,
-    }: { name: keyof State; value: string } = e.target as any;
-
-    this.setState({
-      ...this.state,
-      [name]: value,
-    });
-  };
-
-  handleProjectChange = (e: any) => {
-    const { value }: { value: string } = e.target as any;
-
-    this.setState({
-      project: +value,
-    });
-  };
-
-  handleTemplateChange = (e: any) => {
-    const { value }: { value: string } = e.target as any;
-
-    const template = this.props.templates.find(item => item.id === +value);
-
-    this.setState({
-      timesheetTemplate: template || null,
-    });
-  };
-
-  handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    if (this.state.project === null) {
-      // TODO: should not throw error
+    if (data.project === null) {
       throw new Error('You need to select a project.');
     }
 
-    this.props.onSubmit(this.state, this.state.project);
+    this.props.onSubmit(data, data.project);
   };
 
   render() {
     const { projects, templates } = this.props;
-    const { name } = this.state;
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <Field>
-          <label>Name *</label>
-          <Input
-            placeholder="Name of the group"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-          />
-        </Field>
+      <Form onValidSubmit={this.handleSubmit}>
+        {formState => (
+          <>
+            <Form.Field
+              name="name"
+              label="Name"
+              validations={{ isRequired: true }}
+            >
+              <Input placeholder="Name of the the group" />
+            </Form.Field>
 
-        <Field>
-          <label>Project *</label>
-          <select name="project" onChange={this.handleProjectChange}>
-            <option value="0">Select Project</option>
-            {projects.map(project => (
-              <option key={project.id} value={project.id}>
-                {project.name}
-              </option>
-            ))}
-          </select>
-        </Field>
+            <Form.Field
+              name="project"
+              label="Select Project"
+              validations={{ isRequired: true }}
+            >
+              <Select
+                options={projects.map(project => ({
+                  value: project.id,
+                  label: project.name,
+                }))}
+                placeholder="Select Project"
+              />
+            </Form.Field>
 
-        <Field>
-          <label>Timesheet Template *</label>
-          <select name="timesheetTemplate" onChange={this.handleTemplateChange}>
-            <option value="0">Select Template</option>
-            {templates.map(template => (
-              <option key={template.id} value={template.id}>
-                {template.name}
-              </option>
-            ))}
-          </select>
-        </Field>
+            <Form.Field
+              name="timesheetTemplate"
+              label="Timesheet Template"
+              validations={{ isRequired: true }}
+            >
+              <Select
+                options={templates.map(template => ({
+                  value: template.id,
+                  label: template.name,
+                }))}
+                placeholder="Select Template"
+              />
+            </Form.Field>
 
-        <Button type="submit" color="green">
-          Add
-        </Button>
-      </form>
+            <Button type="submit" color="green" disabled={!formState.isValid}>
+              Add
+            </Button>
+          </>
+        )}
+      </Form>
     );
   }
 }
