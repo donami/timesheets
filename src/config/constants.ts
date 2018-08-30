782a7635b3935f135e0c9ce75b1d360243230736
Markus Hederström <markus.hederstrom@humany.com>
2018-08-30 21:23:02 +0200

    update api url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,9 @@
-export const API_URL = 'http://localhost:9001/api';
+const production = process.env.NODE_ENV === 'production';
+
+const API_URL_PRODUCTION = 'http://46.101.211.180:9001/api';
+const API_URL_DEVELOPMENT = 'http://localhost:9001/api';
+
+export const API_URL = production ? API_URL_PRODUCTION : API_URL_DEVELOPMENT;
 export const STATICS_URL = 'http://localhost:9001';
 
 export const DEFAULT_USER_IMAGE = 'user_default.png';
