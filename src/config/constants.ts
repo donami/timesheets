40f62ebb469db3b0423f0ce173f2d7ff15d935a3
Markus Hederström <markus.hederstrom@humany.com>
2018-08-31 17:28:29 +0200

    fixed statics url

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,13 +1,15 @@
 const production = process.env.NODE_ENV === 'production';
 
-// TODO: remove
-console.log(process.env.NODE_ENV);
-
 const API_URL_PRODUCTION = 'http://46.101.211.180:9001/api';
 const API_URL_DEVELOPMENT = 'http://localhost:9001/api';
 
+const STATICS_URL_PRODUCTION = 'http://46.101.211.180:9001';
+const STATICS_URL_DEVELOPMENT = 'http://localhost:9001';
+
 export const API_URL = production ? API_URL_PRODUCTION : API_URL_DEVELOPMENT;
-export const STATICS_URL = 'http://localhost:9001';
+export const STATICS_URL = production
+  ? STATICS_URL_PRODUCTION
+  : STATICS_URL_DEVELOPMENT;
 
 export const DEFAULT_USER_IMAGE = 'user_default.png';
 export const DEFAULT_USER_IMAGE_FEMALE = 'user_female.png';
